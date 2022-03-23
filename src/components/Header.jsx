8c7c8c8c50aa4e055002703417477c135c1d9f21
Samer A <thedev.samer@gmail.com>
2022-03-23 11:32:29 +0200

    finished the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Header = () => {
   return (
@@ -13,14 +14,17 @@ const Header = () => {
 
         <a href="#">Model X</a>
 
-        <a href="#">Model 3</a>
-
         <a href="#">Model Y</a>
+
+        <a href="#">Model 3</a>
       </Menu>
 
       <RightMenu>
         <a href="#">Shop</a>
+
         <a href="#">Tesla Account</a>
+
+        <CustomMenu />
       </RightMenu>
     </Container>
   );
@@ -36,6 +40,7 @@ const Container = styled.div`
   right: 0;
   display: flex;
   align-items: center;
+  justify-content: space-between;
   padding: 0 20px;
 `;
 
@@ -51,12 +56,23 @@ const Menu = styled.div`
     padding: 0 10px;
     flex-wrap: no-wrap;
   }
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
 `;
 
 const RightMenu = styled.div`
+  display: flex;
+  align-items: center;
+
   a {
     font-weight: 600;
     text-transform: uppercase;
     margin-right: 10px;
   }
+`;
+
+const CustomMenu = styled(MenuIcon)`
+  cursor: pointer;
 `;
