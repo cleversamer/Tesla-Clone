fa5f8874cecb60c1ab8b0774e36a93bdf715b3f8
Samer A <thedev.samer@gmail.com>
2022-03-23 10:19:15 +0200

    fixed layout issues

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -2,7 +2,23 @@ import React from "react";
 import styled from "styled-components";
 
 const Section = () => {
-  return <Wrap>Section</Wrap>;
+  return (
+    <Wrap>
+      <ItemText>
+        <h1>Model S</h1>
+        <p>Order Online for Touchless Delivery</p>
+      </ItemText>
+
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton>Custom Order</LeftButton>
+          <RightButton>Existing Inventory</RightButton>
+        </ButtonGroup>
+
+        <DownArrow src="images/down-arrow.svg" alt="" />
+      </Buttons>
+    </Wrap>
+  );
 };
 
 export default Section;
@@ -10,6 +26,53 @@ export default Section;
 const Wrap = styled.div`
   width: 100vw;
   height: 100vh;
-  background-color: orange;
-  background-image: url(${require("../images/model-s.jpg")});
+  background-image: url(images/model-s.jpg);
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+`;
+
+const Button = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: #fff;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  margin: 8px;
+`;
+
+const LeftButton = styled(Button)``;
+
+const RightButton = styled(Button)``;
+
+const DownArrow = styled.img`
+  margin-top: 20px;
+  height: 40px;
+  animation: animateDown infinite 1.5s;
+`;
+
+const Buttons = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
