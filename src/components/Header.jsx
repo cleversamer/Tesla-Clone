764d13b7d82373f819ff3aa439cc0c335991db37
Samer A <thedev.samer@gmail.com>
2022-03-23 11:16:31 +0200

    created the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,62 @@
 import React from "react";
+import styled from "styled-components";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Container>
+      <a href="index.html">
+        <img src="images/logo.svg" alt="Tesla logo" />
+      </a>
+
+      <Menu>
+        <a href="#">Model S</a>
+
+        <a href="#">Model X</a>
+
+        <a href="#">Model 3</a>
+
+        <a href="#">Model Y</a>
+      </Menu>
+
+      <RightMenu>
+        <a href="#">Shop</a>
+        <a href="#">Tesla Account</a>
+      </RightMenu>
+    </Container>
+  );
 };
 
 export default Header;
+
+const Container = styled.div`
+  min-height: 60px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+`;
+
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-grow: 1;
+
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0 10px;
+    flex-wrap: no-wrap;
+  }
+`;
+
+const RightMenu = styled.div`
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    margin-right: 10px;
+  }
+`;
